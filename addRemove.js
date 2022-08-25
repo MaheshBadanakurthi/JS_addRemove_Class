addd840575b7b629ceb702d55f48ff370b7c46d9
Mahesh <itstribe.mahi@gmail.com>
2022-08-25 21:04:16 +0530

    removing class

--- addRemove.js
+++ addRemove.js
@@ -1,4 +1,6 @@
 function styleErase(){
     document.getElementById("changePara").classList.remove("pcolor");
     document.getElementById("totalBody").classList.add("wholeBody");
+    // document.getElementById("totalBody").classList.append("wholeBody");
+    
 }
